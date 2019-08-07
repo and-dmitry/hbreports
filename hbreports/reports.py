c23c06fb300b4a186ea83f2422c2cf6477522fc2
Dmitry Andreychuk <and.dmitry@gmail.com>
2019-08-07 11:35:55 +0300

    Another take on reports
    I still don't like it.

--- hbreports/reports.py
+++ hbreports/reports.py
@@ -1,28 +1,105 @@
-"""Reports."""
+"""Reports.
+
+Using abbreviations for report names. Full names would be just
+ridiculously long.
+"""
+
+from collections import defaultdict
+
+from sqlalchemy import func
+from sqlalchemy.sql import select
+
+from hbreports.db import (
+    account,
+    transaction,
+)
 
 
-class ReportResult:
-    """Container for report results.
+class ReportTable:
+    """Table with report results.
 
     When you call run() method on a report, you get an instance of
-    this class. Basically is's just a table.
+    this class.
     """
 
     def __init__(self):
-        self.rows = []
+        self._rows = []
+        # Columns number
+        self.width = None
+
+    def add_row(self, iterable):
+        row = tuple(iterable)
+        if self.width and len(row) != self.width:
+            raise ValueError('Wrong number of columns')
+        else:
+            self.width = len(row)
+        self._rows.append(row)
+
+    @property
+    def rows(self):
+        """Get iterator for table rows."""
+        return iter(self._rows)
+
+
+class ReportTable2d:
+
+    def __init__(self):
+        self._table = defaultdict(dict)
+
+    def set_cell(self, row, column, value):
+        self._table[row][column] = value
+
+    @property
+    def rows(self):
+        """Get iterator for table rows."""
+        column_names = sorted({column_name
+                               for row in self._table.values()
+                               for column_name in row.keys()})
+        yield [None] + column_names
+        for row_name in sorted(self._table.keys()):
+            yield [row_name] + [self._table[row_name][column_name]
+                                for column_name in column_names]
+
+
+class TtaReport:
+    """TTA - Total Transactions by Account.
+
+    This is the simpliest report possible. It's actual porpose is to
+    help design the reports framework.
+    """
+
+    name = 'Total transactions quantity by account'
+
+    def run(self, dbc):
+        table = ReportTable()
+        table.add_row(['Accounts', 'Transactions qty.'])
+        result = dbc.execute(
+            select([account.c.name,
+                    func.count(transaction.c.id)])
+            .select_from(account.outerjoin(
+                transaction,
+                # explicit on-clause seems better
+                transaction.c.account_id == account.c.id))
+            .group_by(account.c.name)
+            .order_by(account.c.name)
+        )
+        for row in result:
+            table.add_row(row)
+        return table
 
 
 class AmcReport:
-    """Average Monthly expenses by Category."""
+
+    """AMC - Average Monthly expenses by Category."""
+
+    name = 'Average monthly expenses by category'
 
     def __init__(self, from_year, to_year):
         self._from_year = from_year
         self._to_year = to_year
 
     def run(self, dbc):
-        result = ReportResult()
+        table = ReportTable()
         # We'll start with this:
         # select cat.name, count(*) as cnt, sum(amount) as s from 'transaction' as tr join split on split.transaction_id == tr.id left join category as subcat on subcat.id = split.category_id left join category as cat on cat.id = subcat.parent_id or cat.id = subcat.id and cat.parent_id is null group by cat.name order by cnt desc;
-        result.rows = [[None] + [str(year) for year in range(self._from_year, self._to_year + 1)],
-                       [None, 0.0]]
-        return result
+        return table
--- hbreports/tests/test_reports.py
+++ hbreports/tests/test_reports.py
@@ -1,8 +1,54 @@
+import datetime
+
 import pytest
 from sqlalchemy import create_engine
 
 from hbreports import db
-from hbreports.reports import AmcReport
+from hbreports.reports import (
+    AmcReport,
+    TtaReport,
+    ReportTable,
+    ReportTable2d,
+)
+
+
+# ReportTable tests
+
+
+def test_report_table_empty():
+    table = ReportTable()
+    assert list(table.rows) == []
+
+
+def test_report_table_inconsistent_rows():
+    table = ReportTable()
+    table.add_row([1, 2])
+    with pytest.raises(ValueError):
+        table.add_row([1])
+
+
+def test_report_table_add_row():
+    table = ReportTable()
+    row = (1, 2, 3)
+    iterable = iter(row)
+    table.add_row(iterable)
+
+    rows = list(table.rows)
+    assert len(rows) == 1
+    assert list(rows[0]) == list(row)
+
+
+def test_table2d():
+    """Test 2D table"""
+    table = ReportTable2d()
+    table.set_cell('r1', 'c1', 1)
+    table.set_cell('r1', 'c2', 2)
+    table.set_cell('r2', 'c1', 3)
+    table.set_cell('r2', 'c2', 4)
+    rows = [list(row) for row in table.rows]
+    assert rows == [[None, 'c1', 'c2'],
+                    ['r1', 1, 2],
+                    ['r2', 3, 4]]
 
 
 # TODO: duplication with test_hbfile
@@ -21,15 +67,80 @@ def db_connection(db_engine):
     connection.close()
 
 
-def test_amc_empty(db_connection):
+# TTA report tests
+
+
+def test_tta_empty_db(db_connection):
+    """Test TTA with empty db."""
+    report = TtaReport()
+    table = report.run(db_connection)
+
+    rows = list(table.rows)
+    assert len(rows) == 1
+    header = rows[0]
+    assert isinstance(header[0], str)
+    assert isinstance(header[1], str)
+
+
+def test_tta_no_transactions(db_connection):
+    # There are accounts, but no transactions
+    currency_id = 1
+    db_connection.execute(db.currency.insert().values(
+        id=currency_id,
+        name='currency1'))
+    accounts = [
+        {'name': '1 account', 'currency_id': currency_id},
+        {'name': '2 account', 'currency_id': currency_id}
+    ]
+    db_connection.execute(db.account.insert(), accounts)
+
+    report = TtaReport()
+    table = report.run(db_connection)
+    rows = list(table.rows)
+
+    assert len(rows) == 3
+    header, row1, row2 = table.rows
+    assert list(row1) == [accounts[0]['name'], 0]
+    assert list(row2) == [accounts[1]['name'], 0]
+
+
+def test_tta_basic(db_connection):
+    # TODO: creating these fixtures is tedious. Try factory boy or
+    # something?
+    currency_id = 1
+    db_connection.execute(db.currency.insert().values(
+        id=currency_id,
+        name='currency1'))
+    accounts = [
+        {'id': 1, 'name': '1 account', 'currency_id': currency_id},
+        {'id': 2, 'name': '2 account', 'currency_id': currency_id}
+    ]
+    db_connection.execute(db.account.insert(), accounts)
+    trans = [{'account_id': accounts[0]['id'],
+              'date': datetime.date(2019, 1, 1),
+              'status': 0}] * 3
+    db_connection.execute(db.transaction.insert(), trans)
+
+    report = TtaReport()
+    table = report.run(db_connection)
+    header, row1, row2 = table.rows
+    assert list(row1) == [accounts[0]['name'], len(trans)]
+    assert list(row2) == [accounts[1]['name'], 0]
+
+
+# AMC report tests
+
+
+def test_amc_one_year(db_connection):
     year = 2018
     report = AmcReport(from_year=year,
                        to_year=year)
-    result = report.run(db_connection)
-    # header
-    assert result.rows[0][1] == str(year)
+    table = report.run(db_connection)
+    assert table.width == 2
+    header, row = table.rows
+    assert header[1] == str(year)
     # None represents 'no category', for now
-    assert result.rows[1] == [None, 0.0]
+    assert list(row) == [None, 0.0]
 
 
 def test_amc_years_range(db_connection):
@@ -37,17 +148,7 @@ def test_amc_years_range(db_connection):
     to_year = 2019
     report = AmcReport(from_year=from_year,
                        to_year=to_year)
-    result = report.run(db_connection)
-    assert result.rows[0][1:] == [str(year)
-                                  for year in range(from_year, to_year + 1)]
-
-
-def test_amc_basic(db_connection):
-    # TODO: fill db
-    year = 2018
-    report = AmcReport(from_year=year,
-                       to_year=year)
-    result = report.run(db_connection)
-    # TODO: check sums
-    assert result.rows[0][1] == str(year)
-    assert result.rows[1] == [None, 0.0]
+    table = report.run(db_connection)
+    header, row = table.rows
+    assert header[1:] == [str(year)
+                          for year in range(from_year, to_year + 1)]
