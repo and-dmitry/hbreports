bfe90e0151cf7e345b402d08464f7db815a6129c
Dmitry Andreychuk <and.dmitry@gmail.com>
2019-08-10 10:14:22 +0300

    Fix plain-text table render
    Add newline in the end.

--- hbreports/render.py
+++ hbreports/render.py
@@ -6,9 +6,11 @@ from texttable import Texttable
 class PlainTextRenderer:
     """Rendering in plain text."""
 
+    # TODO: move stream to ctor
     def render_table(self, rows, stream):
         """Print table to file-like object."""
         # TODO: alignment, width
         table = Texttable()
         table.add_rows(rows)
         stream.write(table.draw())
+        stream.write('\n')
