d9a75d0008bd4d040cba3f2452b70bd0e5cd7837
Dmitry Andreychuk <and.dmitry@gmail.com>
2019-08-15 10:08:46 +0300

    Refactor XHB parsing
    Moved parsing to _StreamParser class. Elements are now automatically
    mapped to handlers.

--- hbreports/hbfile.py
+++ hbreports/hbfile.py
@@ -10,7 +10,7 @@ import datetime
 import enum
 import xml.etree.ElementTree as ET
 
-from sqlalchemy.exc import IntegrityError, SQLAlchemyError
+from sqlalchemy.exc import SQLAlchemyError
 
 from hbreports import db
 
@@ -77,78 +77,109 @@ def initial_import(file_object, dbc):
     :param file_object: file-like object with XHB data
     :param sqlalchemy.engine.Connectable dbc: database connection
     """
-    # HomeBamk XML file has no nested elements and forward
-    # references. It's very easy to parse. Even an event-driven
-    # implementation requires no additional effort.
-    for event, elem in ET.iterparse(file_object, events=['start']):
-        try:
-            _process_element(elem, dbc)
-        except SQLAlchemyError as exc:
-            raise DataImportError(
-                f'Failed to import data from "{elem.tag}" element '
-                'due to a database error') from exc
+    parser = _StreamParser(dbc)
+    parser.parse(file_object)
 
 
 # TODO: create some sort of attr-column mapper?
-# TODO: create importer classes?
 # TODO: check file version
 
 
-def _process_currency(elem, dbc):
-    dbc.execute(
-        db.currency.insert().values(
-            id=_get_attr(elem, 'key'),
-            name=_get_attr(elem, 'name')))
+class _StreamParser:
 
+    """Stream parser for HomeBank files.
 
-def _process_account(elem, dbc):
-    dbc.execute(
-        db.account.insert().values(
-            id=_get_attr(elem, 'key'),
-            name=_get_attr(elem, 'name'),
-            currency_id=_get_attr(elem, 'curr')))
+    HomeBamk XML file has no nested elements and forward
+    references. So event-driven parsing is very easy to implement
+    in this case.
 
+    Define method named "_handle_TAG" to handle elements with tag
+    "TAG".
+    """
 
-def _process_payee(elem, dbc):
-    dbc.execute(
-        db.payee.insert().values(
-            id=_get_attr(elem, 'key'),
-            name=_get_attr(elem, 'name')))
+    _HANDLER_PREFIX = '_handle_'
 
+    def __init__(self, db_connection):
+        self._dbc = db_connection
 
-def _process_category(elem, dbc):
-    # TODO: Are subcategories of income categories explicitly marked
-    # as income? We should mark anyway.
-    flags = _get_attr(elem, 'flags')
-    dbc.execute(db.category.insert().values(
-        id=_get_attr(elem, 'key'),
-        name=_get_attr(elem, 'name'),
-        parent_id=_get_attr(elem, 'parent'),
-        income=bool(flags & CategoryFlag.INCOME)))
+    def parse(self, file_object):
+        for event, elem in ET.iterparse(file_object, events=['start']):
+            try:
+                self._do_handle_element(elem)
+            except SQLAlchemyError as exc:
+                raise DataImportError(
+                    f'Failed to import data from "{elem.tag}" element '
+                    'due to a database error') from exc
 
+    def _do_handle_element(self, elem):
+        """Handle XML element.
 
-def _process_transaction(elem, dbc):
-    result = dbc.execute(db.txn.insert().values(
-        date=_get_attr(elem, 'date'),
-        account_id=_get_attr(elem, 'account'),
-        status=_get_attr(elem, 'st'),
-        payee_id=_get_attr(elem, 'payee'),
-        memo=_get_attr(elem, 'wording'),
-        info=_get_attr(elem, 'info'),
-        paymode=_get_attr(elem, 'paymode')
-    ))
-    txn_id = result.inserted_primary_key[0]
+        Name is prefixed to avoid clash with custom handler methods.
+        """
+        try:
+            handler = getattr(self, self._HANDLER_PREFIX + elem.tag)
+        except AttributeError:
+            # ignoring unknown elements
+            pass
+        else:
+            handler(elem)
 
-    # tags
-    for tag in _get_attr(elem, 'tags').split():
-        dbc.execute(db.txn_tag.insert().values(
-            txn_id=txn_id,
-            name=tag))
+    def _handle_cur(self, elem):
+        """Handle currency."""
+        self._dbc.execute(
+            db.currency.insert().values(
+                id=_get_attr(elem, 'key'),
+                name=_get_attr(elem, 'name')))
 
-    if _is_multipart(elem):
-        _process_multipart_transaction(elem, txn_id, dbc)
-    else:
-        _process_simple_transaction(elem, txn_id, dbc)
+    def _handle_account(self, elem):
+        self._dbc.execute(
+            db.account.insert().values(
+                id=_get_attr(elem, 'key'),
+                name=_get_attr(elem, 'name'),
+                currency_id=_get_attr(elem, 'curr')))
+
+    def _handle_pay(self, elem):
+        """Handle payee."""
+        self._dbc.execute(
+            db.payee.insert().values(
+                id=_get_attr(elem, 'key'),
+                name=_get_attr(elem, 'name')))
+
+    def _handle_cat(self, elem):
+        """Handle category."""
+        # TODO: Are subcategories of income categories explicitly marked
+        # as income? We should mark anyway.
+        flags = _get_attr(elem, 'flags')
+        self._dbc.execute(
+            db.category.insert().values(
+                id=_get_attr(elem, 'key'),
+                name=_get_attr(elem, 'name'),
+                parent_id=_get_attr(elem, 'parent'),
+                income=bool(flags & CategoryFlag.INCOME)))
+
+    def _handle_ope(self, elem):
+        """Handle operation (transaction)."""
+        result = self._dbc.execute(
+            db.txn.insert().values(
+                date=_get_attr(elem, 'date'),
+                account_id=_get_attr(elem, 'account'),
+                status=_get_attr(elem, 'st'),
+                payee_id=_get_attr(elem, 'payee'),
+                memo=_get_attr(elem, 'wording'),
+                info=_get_attr(elem, 'info'),
+                paymode=_get_attr(elem, 'paymode')))
+        txn_id = result.inserted_primary_key[0]
+
+        # tags
+        for tag in _get_attr(elem, 'tags').split():
+            self._dbc.execute(db.txn_tag.insert().values(
+                txn_id=txn_id,
+                name=tag))
+
+        if _is_multipart(elem):
+            _process_multipart_transaction(elem, txn_id, self._dbc)
+        else:
+            _process_simple_transaction(elem, txn_id, self._dbc)
 
 
 def _is_multipart(elem):
@@ -258,23 +289,3 @@ def _get_attr(elem, attr_name):
             raise DataImportError(
                 f'Required attribute "{elem.tag}.{attr_name}" not found')
         return info.default
-
-
-_ELEMENT_MAPPING = {
-    'cur': _process_currency,
-    'account': _process_account,
-    'pay': _process_payee,
-    'cat': _process_category,
-    'ope': _process_transaction,
-}
-
-
-def _process_element(elem, dbc):
-    """Process arbitrary element."""
-    try:
-        func = _ELEMENT_MAPPING[elem.tag]
-    except KeyError:
-        # ignore unknown elements
-        pass
-    else:
-        func(elem, dbc)
