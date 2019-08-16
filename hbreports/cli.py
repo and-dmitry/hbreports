and-dmitry/hbreports

commit 53017c83295fb949687d2c1f8bd4257946f6cc20

Change error handling in CLI
Use SystemExit via sys.exit(). It's easier to use, easier to test.

Added more tests for CLI.

hbreports/cli.py
@@ -12,35 +12,29 @@ from hbreports.render import PlainTextRenderer
 
 def handle_import_command(args):
     """Handle "import" command."""
-    # TODO: exceptions?
     if not os.path.exists(args.xhb_path):
-        print('Cannot perform import. '
-              f'HomeBank file "{args.xhb_path}" not found', file=sys.stderr)
-        return 1
+        sys.exit('Cannot perform import. '
+                 f'HomeBank file "{args.xhb_path}" not found.')
 
     if os.path.exists(args.db_path):
-        print('Cannot perform import. '
-              f'Database file "{args.db_path}" already exists',
-              file=sys.stderr)
-        return 1
+        sys.exit('Cannot perform import. '
+                 f'Database file "{args.db_path}" already exists')
 
     engine = db.init_db(args.db_path)
     try:
         with engine.begin() as dbc, open(args.xhb_path) as f:
             initial_import(f, dbc)
     except DataImportError as exc:
-        print('Import failed: ' + str(exc), file=sys.stderr)
-        return 1
-    return 0
+        # TODO: delete db
+        sys.exit('Import failed: ' + str(exc))
 
 
 def handle_report_command(args):
     """Handle "report" command."""
     # TODO: also support xhb (import to memory db)
     if not os.path.exists(args.db_path):
-        print("Can't do a report"
-              f'Database file "{args.db_path}" not found', file=sys.stderr)
-        return 1
+        sys.exit("Can't generate a report. "
+                 f'Database file "{args.db_path}" not found.')
 
     engine = db.init_db(args.db_path)
 
@@ -51,14 +45,12 @@ def handle_report_command(args):
     elif args.report_name == 'abc':
         report_gen = AnnualBalanceByCategory()
     else:
-        print('Unknown report', file=sys.stderr)
-        return 1
+        sys.exit(f'Unknown report "{args.report_name}"')
     with engine.begin() as db_connection:
         report = report_gen.generate_report(db_connection)
     # TODO: select renderer
     renderer = PlainTextRenderer(sys.stdout)
     renderer.render(report)
-    return 0
 
 
 def main(argv):
@@ -82,14 +74,15 @@ def main(argv):
         'report',
         help='compile a report')
     # TODO: move to parent or change to source (db/xhb)
+    # TODO: this is a weird order - report db report_name. Change or use flags.
     report_parser.add_argument('db_path', help='path to sqlite database file')
     report_parser.add_argument('report_name', help='name of report')
     report_parser.set_defaults(func=handle_report_command)
 
     args = parser.parse_args(argv)
     # This is a standard way of handling (sub)commands
-    return args.func(args)
+    args.func(args)
 
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+    main(sys.argv)

hbreports/tests/test_cli.py
@@ -1,3 +1,4 @@
+import pytest
 from sqlalchemy import create_engine
 
 from hbreports.cli import main
@@ -18,9 +19,8 @@ def test_import_success(tmp_path):
         f.write(MINI_XHB)
     db_path = tmp_path / 'test.db'
 
-    status = main(['import', str(xhb_path), str(db_path)])
+    main(['import', str(xhb_path), str(db_path)])
 
-    assert status == 0
     assert db_path.exists()
 
     # Not using hbreports.db module to keep it as simple as possible
@@ -33,12 +33,54 @@ def test_import_success(tmp_path):
         engine.dispose()
 
 
+def test_import_no_xhb_file(tmp_path):
+    """Test import - XHB doesn't exist."""
+    xhb_path = tmp_path / 'test.xhb'
+    db_path = tmp_path / 'test.db'
+
+    with pytest.raises(SystemExit, match='not found'):
+        main(['import', str(xhb_path), str(db_path)])
+
+
+def test_import_db_exists(tmp_path):
+    """Test import - db already exists."""
+    xhb_path = tmp_path / 'test.xhb'
+    with xhb_path.open('w') as f:
+        f.write(MINI_XHB)
+
+    db_path = tmp_path / 'test.db'
+    db_path.touch()
+
+    with pytest.raises(SystemExit, match='exist'):
+        main(['import', str(xhb_path), str(db_path)])
+
+
 def test_import_non_xml_file(tmp_path):
     xhb_path = tmp_path / 'test.xhb'
     with xhb_path.open('w') as f:
         f.write('test')
     db_path = tmp_path / 'test.db'
 
-    status = main(['import', str(xhb_path), str(db_path)])
+    with pytest.raises(SystemExit, match='XML'):
+        main(['import', str(xhb_path), str(db_path)])
 
-    assert status == 1
+
+def test_report_no_db(tmp_path):
+    """Test report - db doesn't exist."""
+    db_path = tmp_path / 'test.db'
+    with pytest.raises(SystemExit, match='not found'):
+        main(['report', str(db_path), 'abc'])
+
+
+def test_report_unknown(tmp_path):
+    """Test unknown report."""
+    db_path = tmp_path / 'test.db'
+    db_path.touch()
+    report_name = 'badname'
+
+    with pytest.raises(SystemExit) as exc_info:
+        main(['report', str(db_path), report_name])
+
+    exc_str = str(exc_info.value).lower()
+    assert report_name in exc_str
+    assert 'unknown' in exc_str

commit 85e8c89c530d44809dcfbecc45ef0fbeccf89b14

Fix CLI args bug
Changes introduced in 53017c8 made CLI unusable. Fixed.

hbreports/cli.py
@@ -54,7 +54,11 @@ def handle_report_command(args):
     renderer.render(report)
 
 
-def main(argv):
+def main(argv=None):
+    """CLI entry point.
+
+    :param list argv: arguments (for testing) or None to use sys.argv
+    """
     # TODO: list supported reports with --help or with special command
     parser = argparse.ArgumentParser(prog='hbreports')
     subparsers = parser.add_subparsers(
@@ -86,4 +90,4 @@ def main(argv):
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()